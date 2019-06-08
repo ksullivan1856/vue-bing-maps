a129549c5c27466da3fad78df8af4bb1abf6ce58
Kenneth Sullivan <Ksullivan1856@Kenneths-MacBook-Pro.local>
2019-06-08 18:37:22 -0400

    Update hello world example package reference

--- examples/hello_world/src/components/HelloWorld.vue
+++ examples/hello_world/src/components/HelloWorld.vue
@@ -1,8 +1,19 @@
 <template>
   <div class="hello">
-    <bing-map credentials="[YOUR_API_KEY]">
-
-    </bing-map>
+    <div class="map-example">
+      <bing-map credentials="[YOUR_API_KEY]" :options="mapOptions">
+        <bing-map-layer name="layer-1" :visible="layerVisible">
+          <bing-map-pushpin v-for="item in pins" 
+                            :key="item.key"
+                            :location="item.location" 
+                            :options="item.options">
+          </bing-map-pushpin>
+        </bing-map-layer>
+      </bing-map>
+    </div>
+    <div>
+      <button type="button" @click="toggleLayer">Toggle Layer Visibility</button>
+    </div>
   </div>
 </template>
 
@@ -13,24 +24,51 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: function(){
+    return {
+      layerVisible: true,
+      mapOptions: {
+        zoom: 4
+      },
+      pins: [
+        {
+          key: 'pin1',
+          location: {
+            latitude: 36.922450,
+            longitude: -104.829098
+          },
+          options: {
+            title: 'pin 1',
+            visible: true
+          }
+        },
+        {
+          key: 'pin2',
+          location: {
+            latitude: 39.950806,
+            longitude: -77.770620
+          },
+          options: {
+            title: 'pin 2',
+            visible: true
+          }
+        }
+      ]
+    };
+  },
+  methods: {
+    toggleLayer() {
+      this.layerVisible = !this.layerVisible;
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.map-example {
+  width: 100%;
+  height: 500px;
 }
 </style>
